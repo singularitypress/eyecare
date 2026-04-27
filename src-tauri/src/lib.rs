edf957372ce9d5511584a29b6685d4e1a5475b17
Jay <jaypandya2k5@gmail.com>
2026-04-26 20:09:07 -0400

    use app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,11 +2,13 @@
 #[cfg(target_os = "windows")]
 use window_vibrancy::apply_acrylic;
 
+use tauri::Manager; 
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .setup(|_app| {
+        .setup(|app| {
             
             // By wrapping this in a block, the window variable is 
             // only instantiated when building for Windows.
@@ -18,12 +20,12 @@ pub fn run() {
             }
 
             // If you eventually add macOS vibrancy, you can add another block here:
-            // #[cfg(target_os = "macos")]
-            // {
-            //     let window = app.get_webview_window("main").unwrap();
-            //     window_vibrancy::apply_vibrancy(&window, window_vibrancy::NSVisualEffectMaterial::HudWindow, None, None)
-            //         .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
-            // }
+            #[cfg(target_os = "macos")]
+            {
+                let window = app.get_webview_window("main").unwrap();
+                window_vibrancy::apply_vibrancy(&window, window_vibrancy::NSVisualEffectMaterial::HudWindow, None, None)
+                    .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+            }
 
             Ok(())
         })
